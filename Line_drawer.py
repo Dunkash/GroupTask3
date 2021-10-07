02e7f3680caa86090bf6c9744975d14c4b4bd08d
Dunkash <45978367+Dunkash@users.noreply.github.com>
2021-10-08 00:54:55 +0300

    Add files via upload

--- Line_drawer.py
+++ Line_drawer.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+from math import floor
 
 class Point:
 
@@ -38,39 +39,73 @@ def lineBrez(beg,end,img):
         t+=1
         img.putpixel((beg.X,beg.Y),(100,100,100,255))
 
+def rfrac(x):
+    return 1-(x%1)
+
 
 def lineVu(beg,end,img):
     if (beg.X>end.X):
         beg.X,end.X = end.X,beg.X
         beg.Y,end.Y = end.Y,beg.Y
-
     delX = end.X - beg.X
     delY = end.Y - beg.Y
+    if ((delX == 0) or (delY == 0)):
+        lineBrez(beg,end,img)
+        return
+    
+    mirror = False
+    if (abs(delX)<abs(delY)):
+        beg.X,beg.Y = beg.Y,beg.X
+        end.X,end.Y = end.Y,end.X
+        delX,delY = delY,delX
+        mirror = True
+
     grad = delY/delX
 
-    yend = beg.Y + grad * (round(beg.X) - beg.X)
-    xgagp = 1 - ((beg.X + 0.5) - int(beg.X + 0.5))
-    xpxl1 = round(beg.X)
-    ypxl1 = int(yend)
-    img.putpixel((xpxl1,ypxl1),(100,100,100,round(255*(1-(yend-int(yend)))*xgagp)))
-    img.putpixel((xpxl1,ypxl1+1),(100,100,100,round(255*(yend-int(yend))*xgagp)))
+    xend = round(beg.X)
+    yend = beg.Y + grad * (xend - beg.X)
+    xgap = rfrac(beg.X + 0.5)
+    xpxl1 = xend
+    ypxl1 = floor(yend)
+    if (not mirror):
+        img.putpixel((xpxl1,ypxl1),(100,100,100,round(255*rfrac(yend)*xgap)))
+        img.putpixel((xpxl1,ypxl1+1),(100,100,100,round(255*(yend%1)*xgap)))
+    else:
+        img.putpixel((ypxl1,xpxl1),(100,100,100,round(255*rfrac(yend)*xgap)))
+        img.putpixel((ypxl1+1,xpxl1),(100,100,100,round(255*(yend%1)*xgap)))
     intery = yend + grad
 
-    yend = end.Y + grad*(round(end.X) - end.X)
-    xgap = (end.X + 0.5) - int(end.X + 0.5)
-    xpxl2 = round(end.X)
-    ypxl2 = int(yend)
-    img.putpixel((xpxl2,ypxl2),(100,100,100,round(255*(1-(yend-int(yend)))*xgap)))
-    img.putpixel((xpxl2,ypxl2+1),(100,100,100,round(255*(yend-int(yend))*xgap)))
+    xend = round(end.X)
+    yend = end.Y + grad * (xend - end.X)
+    xgap = (end.X + 0.5)%1
+    xpxl2 = xend
+    ypxl2 = floor(yend)
+    if (not mirror):
+        img.putpixel((xpxl2,ypxl2),(100,100,100,round(255*rfrac(yend)*xgap)))
+        img.putpixel((xpxl2,ypxl2+1),(100,100,100,round(255*(yend%1)*xgap)))
+    else:
+        img.putpixel((ypxl2,xpxl2),(100,100,100,round(255*rfrac(yend)*xgap)))
+        img.putpixel((ypxl2+1,xpxl2),(100,100,100,round(255*(yend%1)*xgap)))
+    #img.putpixel((xpxl2,ypxl2),(100,100,100,round(255*rfrac(yend)*xgap)))
+    #img.putpixel((xpxl2,ypxl2+1),(100,100,100,round(255*(yend%1)*xgap)))
 
-    for i in range(xpxl1+1,xpxl2):
-        img.putpixel((i,int(intery)),(100,100,100,round(255*(1-(intery - int(intery))))))
-        img.putpixel((i,int(intery)+1),(100,100,100,round(255 * (intery - int(intery)))))
-        intery = intery + grad
+    if (not mirror):
+        for i in range(round(xpxl1),round(xpxl2)):
+            img.putpixel((i,floor(intery)),(100,100,100,round(255*rfrac(intery))))
+            img.putpixel((i,floor(intery)+1),(100,100,100,round(255 * (intery%1))))
+            intery = intery + grad
+
+    else:
+        for i in range(round(xpxl1),round(xpxl2)):
+            img.putpixel((floor(intery),i),(100,100,100,round(255*rfrac(intery))))
+            img.putpixel((floor(intery)+1,i),(100,100,100,round(255 * (intery%1))))
+            intery = intery + grad        
         
-img = Image.new('RGB', (100,100))
+img = Image.new('RGB', (1000,1000))
 img.putalpha(255)
-lineVu(Point(21,10),Point(50,50),img)
-lineVu(Point(20,20),Point(40,40),img)
+lineVu(Point(100,100),Point(400,800),img)
+lineVu(Point(500,500),Point(500,500),img)
+lineBrez(Point(100,200),Point(500,201),img)
+img.save('test.png')
 
 img.show()
